37e5abbc9d105d6eb384e273161db0f8200f84e7
adrhill <adrian.hill@mailbox.org>
2025-09-05 20:12:30 +0200

    Switch to Nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My Yew & Tailwind project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            wasm-pack
+            trunk
+            lld
+            nodejs_24
+            tailwindcss
+          ];
+        };
+      }
+    );
+}
