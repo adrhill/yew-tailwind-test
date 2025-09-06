ed66671f7e39a06b692367841b2133e9b073ac62
adrhill <adrian.hill@mailbox.org>
2025-09-06 03:08:31 +0200

    Update Flake

--- flake.nix
+++ flake.nix
@@ -18,18 +18,56 @@
             latest.cargo
             targets.${rustWasmTarget}.latest.rust-std
           ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
+        rustPlatform =
+          pkgs.makeRustPlatform {
+            rustc = rustToolchain;
+            cargo = rustToolchain;
+          };
+        buildInputs = with pkgs; [
             rustToolchain
-            cargo
+            wasm-bindgen-cli_0_2_100 # match Cargo.toml
             wasm-pack
             trunk
             lld
             nodejs
+            node2nix
             tailwindcss
           ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = buildInputs;
+        };
+        defaultPackage = rustPlatform.buildRustPackage {
+          pname = "yew-nix-build";
+          version = "0.1.1";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            wasm-bindgen-cli_0_2_100 # match Cargo.toml
+            wasm-pack
+            trunk
+            lld
+            nodejs
+            nodePackages.npm
+            tailwindcss
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+
+          cargoLock = { lockFile = ./Cargo.lock; };
+
+          buildPhase = ''
+            set -x
+            echo "Starting npm install..."
+            npm install
+            echo "npm install completed. Starting npx update-browserslist-db..."
+            npx update-browserslist-db@latest
+            echo "npx update-browserslist-db completed. Starting trunk build..."
+            trunk build --release --dist $out         
+            echo "trunk build completed."
+          '';
         };
       }
     );
