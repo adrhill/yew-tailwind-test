e5568cbbfe24fe9a82ebf15575cfda50b07065c6
adrhill <adrian.hill@mailbox.org>
2025-09-08 17:03:36 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Yew & Tailwind project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix.url = "github:nix-community/fenix";
   };
@@ -18,48 +18,36 @@
             latest.cargo
             targets.${rustWasmTarget}.latest.rust-std
           ];
-        rustPlatform =
-          pkgs.makeRustPlatform {
-            rustc = rustToolchain;
-            cargo = rustToolchain;
-          };
-        buildInputs = with pkgs; [
-            rustToolchain
-            wasm-bindgen-cli_0_2_100 # match Cargo.toml
-            wasm-pack
-            trunk
-            lld
-            nodejs
-            node2nix
-            tailwindcss
-          ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = buildInputs;
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rustToolchain;
+          cargo = rustToolchain;
         };
+        buildInputs = with pkgs; [
+          rustToolchain
+          trunk
+          lld
+          nodejs
+          nodePackages.npm
+          nodePackages.autoprefixer
+          nodePackages.postcss
+          nodePackages.tailwindcss
+          tailwindcss
+        ];
+      in {
+        devShells.default = pkgs.mkShell { nativeBuildInputs = buildInputs; };
+
         defaultPackage = rustPlatform.buildRustPackage {
-          pname = "yew-tailwind-test-nix";
+          pname = "wasm-test-nix";
           version = "0.1.0";
           src = ./.;
 
-          nativeBuildInputs = with pkgs; [
-            rustToolchain
-            wasm-bindgen-cli_0_2_100 # match Cargo.toml
-            wasm-pack
-            trunk
-            tailwindcss
-            lld
-            # pkgs.pkg-config
-            # pkgs.openssl
-          ];
-
+          nativeBuildInputs = buildInputs;
           cargoLock = { lockFile = ./Cargo.lock; };
 
+          # npm set prefix ~/.local/lib
           buildPhase = ''
             trunk build --public-url "https://adrianhill.de/wasm-test/" --release --dist $out
           '';
         };
-      }
-    );
+      });
 }
