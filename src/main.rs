61c0fbca14b50b8e6b1d8a7304f0c3d8aa955382
adrhill <adrian.hill@mailbox.org>
2025-09-08 19:59:45 +0200

    Minor CSS tweaks

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,11 @@ struct VideosDetailsProps {
 #[function_component(VideoDetails)]
 fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
     html! {
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <div class="rounded-xl bg-gray-100 dark:bg-gray-800">
             <img class="w-full" src="https://placehold.co/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl  mb-2">{ video.title.clone() }</div>
-                <p class="text-gray-700 text-base">
+            <div class="px-4 py-2">
+                <div class="text-xl font-medium text-black dark:text-white">{ video.title.clone() }</div>
+                <p class="text-gray-500 dark:text-gray-400">
                     { "Video description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil." }
                 </p>
             </div>
@@ -37,7 +37,7 @@ fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
 #[function_component(VideosList)]
 fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
     let on_click = on_click.clone();
-    videos.iter().map(|video| {
+    let list_elements = videos.iter().map(|video| {
         let on_video_select = {
             let on_click = on_click.clone();
             let video = video.clone();
@@ -47,11 +47,19 @@ fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
         };
 
         html! {
-            <div class="mb-2">
+            <div class="text-slate-800 flex w-full items-center rounded-md px-4 transition-all hover:bg-gray-100 dark:hover:bg-gray-800 dark:text-white">
                 <p key={video.id} onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>     
             </div>
         }
-    }).collect()
+    });
+
+    html! {
+        <div class="relative flex flex-col">
+            <nav class="flex w-full flex-col gap-1">
+                {for list_elements}
+            </nav>
+        </div>
+    }
 }
 
 #[function_component(App)]
@@ -98,9 +106,9 @@ fn app() -> Html {
 
     html! {
         <>
-            <div class="prose mx-auto">
-                <h1>{ "WASM Test" }</h1>
-                <h3>{ "Click title for preview:" }</h3>
+            <div class="prose max-w-sm mx-auto py-6">
+                <h1 class="dark:text-white">{ "WASM Test" }</h1>
+                <h3 class="dark:text-white">{ "Click title for preview:" }</h3>
                 <VideosList videos={videos} on_click={on_video_select.clone()} />
                 // Add spacing between the videos list and the details
                 <div class="h-4"></div>
